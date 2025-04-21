93cc367759d9f794d9a49e6afdacd3b266f28764
Maarten de Rijk <maarten.derijk@gmail.com>
2025-04-21 14:45:00 +0200

    feat: arcgis/map-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Container, Paper } from '@mui/material'
-import { useState } from 'react'
-
+import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-locate";
+import { Box, Dialog, DialogContent, Typography } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Container>
-      <Paper>
-        <h1>Vite + React</h1>
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-    </Paper>
-    </Container >
-  )
+    <Box sx={{ height: "100vh", width: "100vw" }}>
+      <Dialog
+        open={false}>
+        <DialogContent>
+          <Typography variant="h6" gutterBottom>
+            This is a dialog box with an ArcGIS map component inside it.
+          </Typography>
+        </DialogContent>
+      </Dialog>
+      <arcgis-map
+        itemId="d5dda743788a4b0688fe48f43ae7beb9"
+      >
+        <arcgis-locate />
+      </arcgis-map>
+    </Box>
+  );
 }
 
 export default App
